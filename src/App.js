5b141f23f89780331c0cf835d18d801c9f0dbb77
Radhikasuresh <radhikasuresh550@gmail.com>
2023-08-09 19:35:15 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import './App.css';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
-import Login from './Components/Login/Login';
-import Chat from './Components/Chat/Chat';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("https://chat-app-backend-radhika.onrender.com/");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
-  </Router>
-  )
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="Your Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
 }
 
 export default App;
